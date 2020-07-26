d9f0cce85673446c79c5bc8a2962e59df68df98e
reqweldzen <nanashiro2015@gmail.com>
2020-07-26 15:48:10 +0900

    Sample

--- BastionModerateApp/Program.cs
+++ BastionModerateApp/Program.cs
@@ -1,12 +1,57 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
 
 namespace BastionModerateApp
 {
-	class Program
+	internal class Program
 	{
-		static void Main(string[] args)
+		private readonly DiscordSocketClient _client;
+		
+		private static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			new Program().MainAsync().GetAwaiter().GetResult();
+		}
+
+		public Program()
+		{
+			_client = new DiscordSocketClient();
+
+			_client.Log += LogAsync;
+			_client.Ready += ReadyAsync;
+			_client.MessageReceived += MessageReceivedAsync;
+		}
+
+		public async Task MainAsync()
+		{
+			await _client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("DiscordToken"));
+			await _client.StartAsync();
+
+			await Task.Delay(Timeout.Infinite);
+		}
+
+		private Task LogAsync(LogMessage log)
+		{
+			Console.WriteLine(log.ToString());
+			return Task.CompletedTask;
+		}
+
+		private Task ReadyAsync()
+		{
+			Console.WriteLine($"{_client.CurrentUser} is connected!");
+
+			return Task.CompletedTask;
+		}
+
+		private async Task MessageReceivedAsync(SocketMessage message)
+		{
+			if (message.Author.Id == _client.CurrentUser.Id)
+				return;
+
+			if (message.Content == "!ping")
+				await message.Channel.SendMessageAsync("pong!");
 		}
 	}
 }
